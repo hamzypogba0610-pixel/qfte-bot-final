6e7bca4630587e45cf24dc2bbb48bd6f4122d23c
hamzypogba0610-pixel <hamzypogba0610@gmail.com>
2026-09-21 21:03:11 +0100

    Update 11_monitor.py

--- qfte_engine/11_monitor.py
+++ qfte_engine/11_monitor.py
@@ -4,9 +4,11 @@ from qfte_engine.historique import sauvegarder_analyse
 def surveiller(data):
     match = data.get("match", {})
     try:
-        sauvegarder_analyse(match, data)
+        entree = sauvegarder_analyse(match, data)
         data["historique_ok"] = True
+        data["analyse_id"] = entree.get("id", "")
     except Exception as e:
         data["historique_ok"] = False
         data["historique_erreur"] = str(e)
+        data["analyse_id"] = ""
     return data
